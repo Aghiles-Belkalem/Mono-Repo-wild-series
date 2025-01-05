22a5b5bee572c61515d4659947ba4d77df6a31c2
Aghiles-Belkalem <belkalemaghiles@gmail.com>
2025-01-05 17:21:22 +0100

    Creation the brows and read for categories and the path

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,14 +9,6 @@ import "../database/checkConnection";
 // Import the Express application from ./app
 import app from "./app";
 
-import type { Request, RequestHandler, Response } from "express";
-
-const sayWelcome: RequestHandler = (req: Request, res: Response) => {
-  res.send("Welcome to Wild Series !");
-};
-
-app.get("/", sayWelcome);
-
 // Get the port from the environment variables
 const port = process.env.APP_PORT;
 
